7cca437f8cf20775875e0e1594df094a1be31c28
Renat145 <abd_renat85@mail.ru>
2024-07-19 13:35:35 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course_name = "Курс: Python,"
+number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+time_per_task = number_of_hours_spent / number_of_homework_completed
+print(course_name, 'всего задач:', number_of_homework_completed, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения:', time_per_task, 'часа.')
